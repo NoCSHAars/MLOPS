bf21595d74a5cc556d05776c2d75e857a692e5e9
louis raffalli <louis@vigisoft.com>
2025-02-19 15:56:31 +0100

    Add dataset encoding fixture and remove obsolete test file

--- mlops/tests/conftest.py
+++ mlops/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from mlops.src.mlops.pipelines.loading.nodes import load_csv_from_bucket
+from mlops.src.mlops.pipelines.processing.nodes import encode_features
 
 
 @pytest.fixture(scope="module")
@@ -16,3 +17,13 @@ def primary_folder():
 @pytest.fixture(scope="module")
 def dataset_not_encoded(project_id, primary_folder):
     return load_csv_from_bucket(project_id, primary_folder)
+
+
+@pytest.fixture(scope="module")
+def test_ratio():
+    return 0.3
+
+
+@pytest.fixture(scope="module")
+def dataset_encoded(dataset_not_encoded):
+    return encode_features(dataset_not_encoded)["features"]
