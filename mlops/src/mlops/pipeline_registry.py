51c5f28469e6a2431e23049ada9809d5c9e5613b
louis raffalli <louis@vigisoft.com>
2025-02-20 15:04:14 +0100

    Add deployment pipeline with model registry integration and environment configuration

--- mlops/src/mlops/pipeline_registry.py
+++ mlops/src/mlops/pipeline_registry.py
@@ -4,6 +4,7 @@ from kedro.pipeline import Pipeline
 from mlops.pipelines.training import pipeline as training_pipeline
 from mlops.pipelines.processing import pipeline as processing_pipeline
 from mlops.pipelines.loading import pipeline as loading_pipeline
+from mlops.pipelines.deployment import pipeline as deployment_pipeline
 from typing import Dict
 
 
@@ -17,10 +18,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     p_processing = processing_pipeline.create_pipeline()
     p_training = training_pipeline.create_pipeline()
     p_loading = loading_pipeline.create_pipeline()
+    p_deployment = deployment_pipeline.create_pipeline()
 
     return {
-        "global": Pipeline([p_loading, p_processing, p_training]),
+        "global": Pipeline([p_loading, p_processing, p_training, p_deployment]),
         "loading": p_loading,
         "processing": p_processing,
         "training": p_training,
+        "deployment": p_deployment,
     }
